2ef2ab13d8cfe697696397ecde483ebe741ce085
harjas-romana <harjas42@icloudcom>
2024-05-05 17:28:36 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import './App.css'
+import "./App.css";
+import HeroSection from "./components/Hero";
+import Navigation from "./components/Navigation";
 const App = () => {
-  return <div>
-    <nav>
-      <div className="logo">
-        <img src="/images/brand_logo.png" alt="logo" />
-      </div>
-      <ul>
-        <li href = '#'>Menu</li>
-        <li href = '#'>Location</li>
-        <li href = '#'>About</li>
-        <li href = '#'>Contact</li>
-      </ul>
-      <button>
-        Login
-      </button>
-    </nav>
-  </div>;
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
 };
 
 export default App;
